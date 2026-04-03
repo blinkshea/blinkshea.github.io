05c9415d966f81a25706c99426c16955d9107380
blinkshea <35117232+blinkshea@users.noreply.github.com>
2026-04-03 17:52:46 -0400

    Replace broken hosted placeholders with real app bundle

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-{{file:C:\Users\BRIAN\Desktop\another optical website\hosted-loader.js}}
+(async function mountHostedApp() {
+  try {
+    const appText = await window.loadCompressedText("./data/app-data.b64.gz.txt");
+    new Function(appText)();
+  } catch (error) {
+    console.error("Unable to load hosted app", error);
+  }
+})();
