165cf5e77a544d73887ef0bc4a71db4050694b91
blinkshea <35117232+blinkshea@users.noreply.github.com>
2026-04-03 17:49:32 -0400

    Refresh hosted app bundle for broader browser support

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-(async function mountHostedApp() {
-  try {
-    const appText = await window.loadCompressedText("./data/app-data.b64.gz.txt");
-    new Function(appText)();
-  } catch (error) {
-    console.error("Unable to load hosted app", error);
-  }
-})();
+{{file:C:\Users\BRIAN\Desktop\another optical website\hosted-loader.js}}
