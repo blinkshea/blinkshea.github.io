1e3f1986aa629de43d42a4a3c64990a7893a8bc6
blinkshea <35117232+blinkshea@users.noreply.github.com>
2026-05-27 09:24:13 -0400

    Publish Lens Book Client from local control center

--- client/data/client-catalog-data.js
+++ client/data/client-catalog-data.js
@@ -1,4 +1,4 @@
 (function(){
-  window.MANDALAY_CLIENT_ACCESS={"mode":"locked","generatedAt":"2026-05-20T10:52:36-04:00","updatedAt":"2026-05-27T09:22:54.5089272-04:00","cacheKey":"d689a239b665d458","title":"Mandalay Lens Book is locked","message":"This client lens book is temporarily locked."};
+  window.MANDALAY_CLIENT_ACCESS={"mode":"locked","generatedAt":"2026-05-20T10:52:36-04:00","updatedAt":"2026-05-27T09:23:43.3365612-04:00","cacheKey":"82e7b9ebac5afaf7","title":"Mandalay Lens Book is locked","message":"This client lens book is temporarily locked."};
   window.MANDALAY_CLIENT_CATALOG=null;
 })();
